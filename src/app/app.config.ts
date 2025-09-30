b4c5721ac54dd66e712b39479e8e7efebb83c224
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2025-09-30 15:58:05 -0300

    feat: define default locale as pt-br for dates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,10 +8,16 @@ import { routes } from './app.routes';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
 import { errorInterceptor } from './shared/interceptors/error.interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+
 dayjs.locale('pt-br');
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor, authInterceptor])),
